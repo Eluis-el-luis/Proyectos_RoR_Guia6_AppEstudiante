3f055a5f35f723fa4029fe8b60e82512674a3172
LuisMeneses <olivasalberto24@gmail.com>
2025-10-07 21:17:54 -0600

    Se realizaron todo los parametros del inciso 2 del propuesto

--- app/models/estudiante.rb
+++ app/models/estudiante.rb
@@ -1,2 +1,7 @@
 class Estudiante < ApplicationRecord
+    # Validar que los campos no estén vacíos
+  validates :nombres, :apellidos, :carrera, :carnet, :fecha_nacimiento, :edad, :celular, presence: true
+
+  # Validar que el celular solo contenga números (enteros)
+  validates :celular, numericality: { only_integer: true }
 end
